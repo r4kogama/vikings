baf5e4f2cbaef65f027259195667e281cb4ae5e0
dmu029018 <dmu029018gmail.com>
2021-05-27 13:40:30 +0200

    vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthArg, strengthArg) {
+        this.health = healthArg;
+        this.strength = strengthArg;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damageArg){
+        //resta a la vida el daño recibido
+        this.health = this.health - damageArg;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(nameArg, healthArg, strengthArg ) {
+        super(healthArg, strengthArg)
+        this.name = nameArg;
+    }
+
+    
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
